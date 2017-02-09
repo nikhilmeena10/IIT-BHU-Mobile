c1c6dbbfd041a9400989fb17504a7fbe707115e5
Nikhil Meena <nikhil.meena.civ14@itbhu.ac.in>
2017-02-09 21:10:28 +0530

    Added four buttons and map functionality
    Self-descriptive

--- app/src/main/java/com/nikhilmeena10/iitbhumobile/MainActivity.java
+++ app/src/main/java/com/nikhilmeena10/iitbhumobile/MainActivity.java
@@ -1,7 +1,13 @@
 package com.nikhilmeena10.iitbhumobile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        //Starting Maps Activity
+        final LinearLayout toMap = (LinearLayout)findViewById(R.id.map_button);
+        toMap.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent geoIntent = new Intent(MainActivity.this, MapsActivity.class);
+                geoIntent.putExtra("latitude",25.262345);
+                geoIntent.putExtra("longitude",82.989608);
+                startActivity(geoIntent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/nikhilmeena10/iitbhumobile/MapsActivity.java
@@ -0,0 +1,53 @@
+package com.nikhilmeena10.iitbhumobile;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static double lat;
+    private static double lng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Intent intent = getIntent();
+        lat = intent.getDoubleExtra("latitude", 0);
+        lng = intent.getDoubleExtra("longitude", 0);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng iitbhu = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(iitbhu).title("IIT (BHU)"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(iitbhu));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 14.0f ) );
+    }
+}
